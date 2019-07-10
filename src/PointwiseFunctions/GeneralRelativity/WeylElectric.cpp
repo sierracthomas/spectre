fa7eba8aaff7d48b09a0d984e0e95ff354b7bbf0
Sierra Thomas <sierra.c.thomas@gmail.com>
2019-07-10 15:38:03 -0700

    Add compiling version

--- src/PointwiseFunctions/GeneralRelativity/WeylElectric.cpp
+++ src/PointwiseFunctions/GeneralRelativity/WeylElectric.cpp
@@ -13,24 +13,25 @@ tnsr::ii<DataType, SpatialDim, Frame> weyl_electric(
     const tnsr::ii<DataType, SpatialDim, Frame>& spatial_ricci,
     const tnsr::ii<DataType, SpatialDim, Frame>& extrinsic_curvature,
     const tnsr::II<DataType, SpatialDim, Frame>&
-        inverse_spatial_metric) noexcept;
-auto weyl =
-    make_with_value<tnsr::ii<DataType, SpatialDim, Frame>>(spatial_ricci, 0.);
-constexpr auto dimensionality = index_dim<0>(weyl);
-for (size_t i = 0; i < dimensionality; ++i) {
-  for (size_t j = i; j < dimensionality; ++j) {
-    weyl.get(i, j) += spatial_ricci.get(i, j);
+        inverse_spatial_metric) noexcept {
+  auto weyl =
+      make_with_value<tnsr::ii<DataType, SpatialDim, Frame>>(spatial_ricci, 0.);
+  constexpr auto dimensionality = index_dim<0>(weyl);
+  for (size_t i = 0; i < dimensionality; ++i) {
+    for (size_t j = i; j < dimensionality; ++j) {
+      weyl.get(i, j) += spatial_ricci.get(i, j);
 
-    for (size_t k = 0; k < dimensionality; ++k) {
-      for (size_t l = k; l < dimensionality; ++l) {
-        weyl.get(i, j) += extrinsic_curvature.get(k, l) *
-                          inverse_spatial_metric.get(k, l) *
-                          extrinsic_curvature.get(i, j);
+      for (size_t k = 0; k < dimensionality; ++k) {
+        for (size_t l = k; l < dimensionality; ++l) {
+          weyl.get(i, j) += extrinsic_curvature.get(k, l) *
+                            inverse_spatial_metric.get(k, l) *
+                            extrinsic_curvature.get(i, j);
 
-        for (size_t m = 0; m < dimensionality; ++m) {
-          weyl.get(i, j) += extrinsic_curvature.get(i, l) *
-                            inverse_spatial_metric.get(m, l) *
-                            extrinsic_curvature.get(m, j);
+          for (size_t m = 0; m < dimensionality; ++m) {
+            weyl.get(i, j) += extrinsic_curvature.get(i, l) *
+                              inverse_spatial_metric.get(m, l) *
+                              extrinsic_curvature.get(m, j);
+          }
         }
       }
     }
@@ -45,16 +46,16 @@ for (size_t i = 0; i < dimensionality; ++i) {
 #define DTYPE(data) BOOST_PP_TUPLE_ELEM(1, data)
 #define FRAME(data) BOOST_PP_TUPLE_ELEM(2, data)
 
-#define INSTANTIATE(_, data)
-template tnsr::ii<DTYPE(data), DIM(data), FRAME(data)> gr::weyl_electric(
-    const tnsr::ii<DTYPE(data), DIM(data), FRAME(data)>& spatial_ricci,
-    const tnsr::ii<DTYPE(data), DIM(data), FRAME(data)>& extrinsic_curvature,
-    const tnsr::II<DTYPE(data), DIM(data), FRAME(data)>&
-        inverse_spatial_metric) noexcept;
+#define INSTANTIATE(_, data)                                                \
+  template tnsr::ii<DTYPE(data), DIM(data), FRAME(data)> gr::weyl_electric( \
+      const tnsr::ii<DTYPE(data), DIM(data), FRAME(data)>& spatial_ricci,   \
+      const tnsr::ii<DTYPE(data), DIM(data), FRAME(data)>&                  \
+          extrinsic_curvature,                                              \
+      const tnsr::II<DTYPE(data), DIM(data), FRAME(data)>&                  \
+          inverse_spatial_metric) noexcept;
 
-GENERATE_INSTANTIATIONS(INSTANTIATE, (1, 2), (double, DataVector),
-                        (Frame::Grid, Frame::Inertial),
-                        (IndexType::Spatial)
+GENERATE_INSTANTIATIONS(INSTANTIATE, (1, 2, 3), (double, DataVector),
+                        (Frame::Grid, Frame::Inertial), (IndexType::Spatial))
 #undef DIM
 #undef DTYPE
 #undef FRAME

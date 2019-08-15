585481ca3519a22cc3cedeab6b8d0c50dc9c6ab2
sierra.c.thomas@gmail.com <sierra.c.thomas@gmail.com>
2019-08-14 19:33:19 -0700

    Added pass-by-value and pass-by-reference versions of WeylElectric

--- src/PointwiseFunctions/GeneralRelativity/WeylElectric.cpp
+++ src/PointwiseFunctions/GeneralRelativity/WeylElectric.cpp
@@ -5,6 +5,7 @@
 
 #include "DataStructures/Tensor/Tensor.hpp"
 #include "Utilities/GenerateInstantiations.hpp"
+#include "Utilities/Gsl.hpp"
 #include "Utilities/MakeWithValue.hpp"
 
 namespace gr {
@@ -14,25 +15,44 @@ tnsr::ii<DataType, SpatialDim, Frame> weyl_electric(
     const tnsr::ii<DataType, SpatialDim, Frame>& extrinsic_curvature,
     const tnsr::II<DataType, SpatialDim, Frame>&
         inverse_spatial_metric) noexcept {
-  auto weyl =
+  // tnsr::ii<DataType, SpatialDim, Frame> weyl_electric_part{
+  // get<0,0>(inverse_spatial_metric).size()};
+  auto weyl_electric_part =
       make_with_value<tnsr::ii<DataType, SpatialDim, Frame>>(spatial_ricci, 0.);
-  constexpr auto dimensionality = index_dim<0>(weyl);
-  for (size_t i = 0; i < dimensionality; ++i) {
-    for (size_t j = i; j < dimensionality; ++j) {
-      weyl.get(i, j) += spatial_ricci.get(i, j);
 
-      for (size_t k = 0; k < dimensionality; ++k) {
-        for (size_t l = 0; l < dimensionality; ++l) {
-          weyl.get(i, j) +=
+  weyl_electric(make_not_null(&weyl_electric_part), spatial_ricci,
+                extrinsic_curvature, inverse_spatial_metric);
+  return weyl_electric_part;
+}
+
+template <size_t SpatialDim, typename Frame, typename DataType>
+void weyl_electric(
+    const gsl::not_null<tnsr::ii<DataType, SpatialDim, Frame>*>
+        weyl_electric_part,
+    const tnsr::ii<DataType, SpatialDim, Frame>& spatial_ricci,
+    const tnsr::ii<DataType, SpatialDim, Frame>& extrinsic_curvature,
+    const tnsr::II<DataType, SpatialDim, Frame>&
+        inverse_spatial_metric) noexcept {
+  //  for(auto& tensor_component : *weyl_electric_part) {
+  // tensor_component.destructive_resize(
+  // get<0,
+  // 0>(inverse_spatial_metric).size());
+
+  for (size_t i = 0; i < SpatialDim; ++i) {
+    for (size_t j = i; j < SpatialDim; ++j) {
+      weyl_electric_part->get(i, j) = spatial_ricci.get(i, j);
+
+      for (size_t k = 0; k < SpatialDim; ++k) {
+        for (size_t l = 0; l < SpatialDim; ++l) {
+          weyl_electric_part->get(i, j) +=
               extrinsic_curvature.get(k, l) * inverse_spatial_metric.get(k, l) *
                   extrinsic_curvature.get(i, j) -
               extrinsic_curvature.get(i, l) * inverse_spatial_metric.get(k, l) *
                   extrinsic_curvature.get(k, j);
-       }
+        }
       }
     }
   }
-  return weyl;
 }
 }  // namespace gr
 
@@ -48,10 +68,18 @@ tnsr::ii<DataType, SpatialDim, Frame> weyl_electric(
       const tnsr::ii<DTYPE(data), DIM(data), FRAME(data)>&                  \
           extrinsic_curvature,                                              \
       const tnsr::II<DTYPE(data), DIM(data), FRAME(data)>&                  \
+          inverse_spatial_metric) noexcept;                                 \
+  template void gr::weyl_electric(                                          \
+      const gsl::not_null<tnsr::ii<DTYPE(data), DIM(data), FRAME(data)>*>   \
+          weyl_electric_part,                                               \
+      const tnsr::ii<DTYPE(data), DIM(data), FRAME(data)>& spatial_ricci,   \
+      const tnsr::ii<DTYPE(data), DIM(data), FRAME(data)>&                  \
+          extrinsic_curvature,                                              \
+      const tnsr::II<DTYPE(data), DIM(data), FRAME(data)>&                  \
           inverse_spatial_metric) noexcept;
 
 GENERATE_INSTANTIATIONS(INSTANTIATE, (1, 2, 3), (double, DataVector),
-                        (Frame::Grid, Frame::Inertial), (IndexType::Spatial))
+                        (Frame::Grid, Frame::Inertial))
 #undef DIM
 #undef DTYPE
 #undef FRAME
--- src/PointwiseFunctions/GeneralRelativity/WeylElectric.hpp
+++ src/PointwiseFunctions/GeneralRelativity/WeylElectric.hpp
@@ -7,6 +7,8 @@
 
 #include "DataStructures/Tensor/TypeAliases.hpp"
 
+#include "Utilities/Gsl.hpp"
+
 namespace gr {
 
 /*!
@@ -14,9 +16,12 @@ namespace gr {
  * \brief Computes the electric part of the Weyl tensor in vacuum.
  *
  * \details Computes the electric part of the Weyl tensor in vacuum \f$E_{ij}\f$
- * as: \f$ E_{ij} = R_{ij} + KK_{ij} - K^m_{i}K_{mj}\f$ where \f$\R_{ij}\f$ is
+ * as: \f$ E_{ij} = R_{ij} + KK_{ij} - K^m_{i}K_{mj}\f$ where \f$R_{ij}\f$ is
  * the spatial Ricci tensor, \f$K_{ij}\f$ is the extrinsic curvature, and
- * \f$K\f$ is the trace of \f$K_{ij}\f$.
+ * \f$K\f$ is the trace of \f$K_{ij}\f$. An additional definition is E_{ij} =
+ * n^a n^b C_{a i b j}, where n is the unit-normal to the hypersurface and C is
+ * the Weyl tensor consistent with the conventions in \cite Boyle2019kee.
+ * \note This needs additional terms for computations in a non-vacuum.
  */
 template <size_t SpatialDim, typename Frame, typename DataType>
 tnsr::ii<DataType, SpatialDim, Frame> weyl_electric(
@@ -25,4 +30,11 @@ tnsr::ii<DataType, SpatialDim, Frame> weyl_electric(
     const tnsr::II<DataType, SpatialDim, Frame>&
         inverse_spatial_metric) noexcept;
 
+template <size_t SpatialDim, typename Frame, typename DataType>
+void weyl_electric(
+    gsl::not_null<tnsr::ii<DataType, SpatialDim, Frame>*> weyl_electric_part,
+    const tnsr::ii<DataType, SpatialDim, Frame>& spatial_ricci,
+    const tnsr::ii<DataType, SpatialDim, Frame>& extrinsic_curvature,
+    const tnsr::II<DataType, SpatialDim, Frame>&
+        inverse_spatial_metric) noexcept;
 }  // namespace gr

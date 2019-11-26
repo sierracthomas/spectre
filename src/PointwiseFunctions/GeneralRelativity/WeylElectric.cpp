171e0733734e59a70352f7157ab59c1de981a76d
sierra.c.thomas@gmail.com <sierra.c.thomas@gmail.com>
2019-11-26 10:21:39 -0800

    Some changes

--- src/PointwiseFunctions/GeneralRelativity/WeylElectric.cpp
+++ src/PointwiseFunctions/GeneralRelativity/WeylElectric.cpp
@@ -52,8 +52,9 @@ void weyl_electric(
 
 template <size_t SpatialDim, typename Frame, typename DataType>
 Scalar<DataType> weyl_electric_scalar(
-    const tnsr::II<DataType, SpatialDim, Frame>& inverse_spatial_metric,
-    const tnsr::ii<DataType, SpatialDim, Frame>& weyl_electric) noexcept {
+    const tnsr::ii<DataType, SpatialDim, Frame>& weyl_electric,
+    const tnsr::II<DataType, SpatialDim, Frame>&
+        inverse_spatial_metric) noexcept {
   auto weyl_electric_scalar =
       make_with_value<Scalar<DataType>>(get<0, 0>(inverse_spatial_metric), 0.0);
   for (size_t i = 0; i < SpatialDim; ++i) {
@@ -96,13 +97,14 @@ Scalar<DataType> weyl_electric_scalar(
 
 GENERATE_INSTANTIATIONS(INSTANTIATE, (1, 2, 3), (double, DataVector),
                         (Frame::Grid, Frame::Inertial))
+
 #define INSTANTIATE_SCALAR(_, data)                                       \
   template Scalar<DTYPE(data)> gr::weyl_electric_scalar(                  \
       const tnsr::ii<DTYPE(data), DIM(data), FRAME(data)>& weyl_electric, \
       const tnsr::II<DTYPE(data), DIM(data), FRAME(data)>&                \
           inverse_spatial_metric) noexcept;
 
-GENERATE_INSTANTIATIONS(INSTANTIATE_SCALAR, (2), (double, DataVector),
+GENERATE_INSTANTIATIONS(INSTANTIATE_SCALAR, (1), (double, DataVector),
                         (Frame::Grid, Frame::Inertial))
 
 #undef DIM

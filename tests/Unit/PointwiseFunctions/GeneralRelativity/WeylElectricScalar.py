9fcb9ea2dde88082081a6fdb41ddb9b0f158c0f9
sierra.c.thomas@gmail.com <sierra.c.thomas@gmail.com>
2019-12-06 13:00:09 -0800

    More changes

--- tests/Unit/PointwiseFunctions/GeneralRelativity/WeylElectricScalar.py
+++ tests/Unit/PointwiseFunctions/GeneralRelativity/WeylElectricScalar.py
@@ -2,9 +2,9 @@
 # See LICENSE.txt for details.
 import numpy as np
 
-#def weyl_electric_scalar(weyl_electric, inverse_spatial_metric):
-#    return (np.einsum("ij,kl,ik,jl", weyl_electric, weyl_electric,
-#                     inverse_spatial_metric, inverse_spatial_metric))
 def weyl_electric_scalar(weyl_electric, inverse_spatial_metric):
-    return (np.einsum("ij, ik", weyl_electric, inverse_spatial_metric) *
-            np.einsum("kl, jl", weyl_electric, inverse_spatial_metric))
+    return (np.einsum("ij,kl,ik,jl", weyl_electric, weyl_electric,
+                inverse_spatial_metric, inverse_spatial_metric))
+#def weyl_electric_scalar(weyl_electric, inverse_spatial_metric):
+#    return (np.einsum("ij, ik", weyl_electric, inverse_spatial_metric) *
+#            np.einsum("kl, jl", weyl_electric, inverse_spatial_metric))

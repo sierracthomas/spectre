032a3b7b30bcfd8dea93eb2c84039436030456f8
sierra.c.thomas@gmail.com <sierra.c.thomas@gmail.com>
2019-12-20 15:49:34 -0800

    Add alternate tests

--- tests/Unit/PointwiseFunctions/GeneralRelativity/WeylElectricScalar.py
+++ tests/Unit/PointwiseFunctions/GeneralRelativity/WeylElectricScalar.py
@@ -2,9 +2,15 @@
 # See LICENSE.txt for details.
 import numpy as np
 
-def weyl_electric_scalar(weyl_electric, inverse_spatial_metric):
-    return (np.einsum("ij,kl,ik,jl", weyl_electric, weyl_electric,
-                     inverse_spatial_metric, inverse_spatial_metric))
+# does not pass all tests
 #def weyl_electric_scalar(weyl_electric, inverse_spatial_metric):
-#    return (np.einsum("ij, ik", weyl_electric, inverse_spatial_metric) *
-#            np.einsum("kl, jl", weyl_electric, inverse_spatial_metric))
+#    return (np.einsum("ij,kl,ik,jl", weyl_electric, weyl_electric,
+#                     inverse_spatial_metric, inverse_spatial_metric))
+# does not pass all tests
+#def weyl_electric_scalar(weyl_electric, inverse_spatial_metric):
+#    return (np.einsum("ij, ij", weyl_electric, inverse_spatial_metric) *
+#            np.einsum("kl, kl", weyl_electric, inverse_spatial_metric))
+# creates wrong-sized resulting tensor
+def weyl_electric_scalar(weyl_electric, inverse_spatial_metric):
+    return (np.einsum("ij, kl", weyl_electric, weyl_electric) *
+            np.einsum("ik, jl", inverse_spatial_metric, inverse_spatial_metric))

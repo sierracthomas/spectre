eb75eed8bc929c68b23691a24f923d8e940924a9
Geoffrey Lovelace <geoffrey4444@gmail.com>
2020-01-16 22:15:34 +0000

    Add perturbed black hole exec

--- src/Evolution/Executables/GeneralizedHarmonic/EvolveGeneralizedHarmonic.hpp
+++ src/Evolution/Executables/GeneralizedHarmonic/EvolveGeneralizedHarmonic.hpp
@@ -19,11 +19,14 @@
 #include "Evolution/Initialization/DiscontinuousGalerkin.hpp"
 #include "Evolution/Initialization/Evolution.hpp"
 #include "Evolution/Initialization/NonconservativeSystem.hpp"
+#include "Evolution/NumericalInitialData.hpp"
 #include "Evolution/Systems/GeneralizedHarmonic/Equations.hpp"
 #include "Evolution/Systems/GeneralizedHarmonic/Initialize.hpp"
 #include "Evolution/Systems/GeneralizedHarmonic/System.hpp"
 #include "Evolution/Systems/GeneralizedHarmonic/Tags.hpp"
-#include "IO/Observer/Actions.hpp"
+#include "IO/DataImporter/DataFileReader.hpp"
+#include "IO/DataImporter/ElementActions.hpp"
+#include "IO/Observer/Actions.hpp"  // IWYU pragma: keep
 #include "IO/Observer/Helpers.hpp"
 #include "IO/Observer/ObserverComponent.hpp"
 #include "IO/Observer/RegisterObservers.hpp"
@@ -111,9 +114,18 @@ struct EvolutionMetavars {
   using system = GeneralizedHarmonic::System<volume_dim>;
   using temporal_id = Tags::TimeStepId;
   static constexpr bool local_time_stepping = false;
-  using initial_data_tag = Tags::AnalyticSolution<
-      GeneralizedHarmonic::Solutions::WrappedGr<gr::Solutions::KerrSchild>>;
+
+  using analytic_solution =
+      GeneralizedHarmonic::Solutions::WrappedGr<gr::Solutions::KerrSchild>;
+  using analytic_solution_tag = Tags::AnalyticSolution<analytic_solution>;
+  using initial_data_tag = Tags::AnalyticSolution<analytic_solution>;
   using boundary_condition_tag = initial_data_tag;
+
+  // The type of initial data for the evolution. Set to `analytic_solution` for
+  // starting from an analytic solution, or `NumericalInitialData` to read
+  // data from the disk.
+  using initial_data = NumericalInitialData<system>;
+
   using normal_dot_numerical_flux =
       Tags::NumericalFlux<GeneralizedHarmonic::UpwindFlux<volume_dim>>;
 
@@ -234,6 +246,7 @@ struct EvolutionMetavars {
     Initialization,
     InitializeTimeStepperHistory,
     Register,
+    ImportData,
     Evolve,
     Exit
   };
@@ -295,6 +308,9 @@ struct EvolutionMetavars {
       observers::ObserverWriter<EvolutionMetavars>,
       intrp::Interpolator<EvolutionMetavars>,
       intrp::InterpolationTarget<EvolutionMetavars, Horizon>,
+      tmpl::conditional_t<is_numerical_initial_data_v<initial_data>,
+                          importer::DataFileReader<EvolutionMetavars>,
+                          tmpl::list<>>,
       DgElementArray<
           EvolutionMetavars,
           tmpl::list<
@@ -307,17 +323,21 @@ struct EvolutionMetavars {
 
               Parallel::PhaseActions<
                   Phase, Phase::Register,
-                  tmpl::flatten<tmpl::list<
-                             intrp::Actions::RegisterElementWithInterpolator,
+                  tmpl::list<intrp::Actions::RegisterElementWithInterpolator,
                              observers::Actions::RegisterWithObservers<
                                  observers::RegisterObservers<
                                      Tags::Time, element_observation_type>>,
-                             Parallel::Actions::TerminatePhase>>>,
+                             tmpl::conditional_t<
+                                 is_numerical_initial_data_v<initial_data>,
+                                 importer::Actions::RegisterWithImporter,
+                                 tmpl::list<>>,
+                             Parallel::Actions::TerminatePhase>>,
+
               Parallel::PhaseActions<
                   Phase, Phase::Evolve,
                   tmpl::list<Actions::RunEventsAndTriggers,
-                             Actions::ChangeSlabSize,
-                             step_actions, Actions::AdvanceTime>>>>>;
+                             Actions::ChangeSlabSize, step_actions,
+                             Actions::AdvanceTime>>>>>;
 
   static constexpr OptionString help{
       "Evolve a generalized harmonic analytic solution.\n\n"
@@ -334,6 +354,9 @@ struct EvolutionMetavars {
       case Phase::InitializeTimeStepperHistory:
         return Phase::Register;
       case Phase::Register:
+        return is_numerical_initial_data_v<initial_data> ? Phase::ImportData
+                                                         : Phase::Evolve;
+      case Phase::ImportData:
         return Phase::Evolve;
       case Phase::Evolve:
         return Phase::Exit;
